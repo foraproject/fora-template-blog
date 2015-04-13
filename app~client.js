c51d83197eac8e08a743d6c2531246b3b511ae1d
Jeswin <jeswinpk@agilehead.com>
2015-04-13 16:59:04 +0530

    ...

--- app~client.js
+++ app~client.js
@@ -1,3 +1,14 @@
+import Isotropy from "isotropy";
 import routes from "./routes";
+import config from "./config";
+import layout from "./layout";
 
-console.log("serving"); 
+var isotropy = new Isotropy();
+
+isotropy.init({
+    staticDirectories: ["public", "js", "vendor", "css", "images", "fonts"],
+    config: config,
+    routes: routes,
+    layout: layout,
+    cb: function(result) { console.log(`Blog started on ${result.host}:${result.port}.`); }
+}).then(function() {}, function(err) { console.log(err.stack); });
